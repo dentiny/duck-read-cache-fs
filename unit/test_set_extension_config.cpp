940293d94e9bd50b319ee98045292fa8cbdd9567
DouEnergy <douenergy@protonmail.com>
2025-02-09 17:57:09 +0800

    use StringUtil::Format

--- unit/test_set_extension_config.cpp
+++ unit/test_set_extension_config.cpp
@@ -21,11 +21,10 @@ const std::string TEST_ON_DISK_CACHE_DIRECTORY =
     "/tmp/duckdb_test_cached_http_cache";
 const std::string TEST_SECOND_ON_DISK_CACHE_DIRECTORY =
     "/tmp/duckdb_test_cached_http_cache_second";
-const std::string TEST_ON_DISK_CACHE_FILE = "'/tmp/test-config.parquet'";
+const std::string TEST_ON_DISK_CACHE_FILE = "/tmp/test-config.parquet";
 
 } // namespace
 
-// One chunk is involved, requested bytes include only "first and last chunk".
 TEST_CASE("Test on changing extension config"
           "change defaul cache dir path setting",
           "[extension config test]") {
@@ -37,26 +36,33 @@ TEST_CASE("Test on changing extension config"
       LocalFileSystem::CreateLocal(), OnDiskCacheConfig{}));
 
   Connection con(db);
-  con.Query("SET cached_http_cache_directory='" + TEST_ON_DISK_CACHE_DIRECTORY +
-            "'");
+  con.Query("SET cached_http_cache_directory=" +
+            StringUtil::Format("'%s'", TEST_ON_DISK_CACHE_DIRECTORY));
   con.Query("CREATE TABLE integers AS SELECT i, i+1 as j FROM range(10) r(i)");
-  con.Query("COPY integers TO" + TEST_ON_DISK_CACHE_FILE);
+  con.Query("COPY integers TO" +
+            StringUtil::Format("'%s'", TEST_ON_DISK_CACHE_FILE));
 
-  // make sure the cache directory is empty before the query
+  // Ensure the cache directory is empty before executing the query.
   int files = GetFileCountUnder(TEST_ON_DISK_CACHE_DIRECTORY);
   REQUIRE(files == 0);
 
-  con.Query("SELECT * FROM" + TEST_ON_DISK_CACHE_FILE);
+  con.Query("SELECT * FROM" +
+            StringUtil::Format("'%s'", TEST_ON_DISK_CACHE_FILE));
 
+  // After executing the query, the cache directory should have one cache file.
   int files_after_query = GetFileCountUnder(TEST_ON_DISK_CACHE_DIRECTORY);
   vector<string> files_in_cache =
       GetSortedFilesUnder(TEST_ON_DISK_CACHE_DIRECTORY);
   REQUIRE(files_after_query == 1);
 
-  con.Query("SET cached_http_cache_directory='" +
-            TEST_SECOND_ON_DISK_CACHE_DIRECTORY + "'");
-  con.Query("SELECT * FROM" + TEST_ON_DISK_CACHE_FILE);
+  // Change the cache directory path and execute the query again.
+  con.Query("SET cached_http_cache_directory=" +
+            StringUtil::Format("'%s'", TEST_SECOND_ON_DISK_CACHE_DIRECTORY));
+  con.Query("SELECT * FROM" +
+            StringUtil::Format("'%s'", TEST_ON_DISK_CACHE_FILE));
 
+  // After executing the query, the NEW directory should have one cache file.
+  // Both directories should have the same cache file.
   const int files_after_query_second =
       GetFileCountUnder(TEST_SECOND_ON_DISK_CACHE_DIRECTORY);
   vector<string> files_in_cache_second =
@@ -64,16 +70,11 @@ TEST_CASE("Test on changing extension config"
   REQUIRE(files_after_query_second == 1);
   REQUIRE(files_in_cache == files_in_cache_second);
 
+  // Clean up
   auto local_filesystem = LocalFileSystem::CreateLocal();
   local_filesystem->RemoveDirectory(TEST_ON_DISK_CACHE_DIRECTORY);
   local_filesystem->RemoveDirectory(TEST_SECOND_ON_DISK_CACHE_DIRECTORY);
-  string temp_file_withoud_single_quote =
-      TEST_ON_DISK_CACHE_FILE; // make a copy first
-  temp_file_withoud_single_quote.erase(
-      std::remove(temp_file_withoud_single_quote.begin(),
-                  temp_file_withoud_single_quote.end(), '\''),
-      temp_file_withoud_single_quote.end());
-  local_filesystem->RemoveFile(temp_file_withoud_single_quote);
+  local_filesystem->RemoveFile(TEST_ON_DISK_CACHE_FILE);
 };
 
 int main(int argc, char **argv) {
