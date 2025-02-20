8c0359b2b4a40156f167cf521f01375619d3dc3b
dentiny <dentinyhao@gmail.com>
2025-02-20 08:19:06 +0000

    fix benchmark and update read and request size

--- benchmark/lineitem_sequential_read.cpp
+++ benchmark/lineitem_sequential_read.cpp
@@ -3,6 +3,7 @@
 #include "duckdb/main/client_context_file_opener.hpp"
 #include "s3fs.hpp"
 #include "scope_guard.hpp"
+#include "time_utils.hpp"
 
 #include <csignal>
 #include <array>
@@ -12,6 +13,13 @@ namespace duckdb {
 
 namespace {
 
+struct BenchmarkSetup {
+	std::string cache_type;
+	std::string profile_type;
+	std::string disk_cache_directory;
+	uint64_t block_size = DEFAULT_CACHE_BLOCK_SIZE;
+};
+
 void SetConfig(case_insensitive_map_t<Value> &setting, char *env_key, char *secret_key) {
 	const char *env_val = getenv(env_key);
 	if (env_val == nullptr) {
@@ -20,61 +28,48 @@ void SetConfig(case_insensitive_map_t<Value> &setting, char *env_key, char *secr
 	setting[secret_key] = Value(env_val);
 }
 
-void SetSecretConfig(shared_ptr<ClientContext> ctx) {
-   auto &set_vars = ctx->config.set_variables;
+void SetOpenerConfig(shared_ptr<ClientContext> ctx, const BenchmarkSetup &benchmark_setup) {
+	auto &set_vars = ctx->config.set_variables;
 	SetConfig(set_vars, "AWS_DEFAULT_REGION", "s3_region");
 	SetConfig(set_vars, "AWS_ACCESS_KEY_ID", "s3_access_key_id");
 	SetConfig(set_vars, "AWS_SECRET_ACCESS_KEY", "s3_secret_access_key");
+	set_vars["cached_http_profile_type"] = Value(benchmark_setup.profile_type);
+	set_vars["cached_http_type"] = Value(benchmark_setup.cache_type);
+	set_vars["cached_http_cache_directory"] = Value(benchmark_setup.disk_cache_directory);
+	set_vars["cached_http_cache_block_size"] = Value::UBIGINT(benchmark_setup.block_size);
 }
 
-void TestSequentialRead() {	
-    DuckDB db {};
-    StandardBufferManager buffer_manager {*db.instance, "/tmp/cached_http_fs_benchmark"};
-    auto s3fs = make_uniq<S3FileSystem>(buffer_manager);
+void TestSequentialRead(const BenchmarkSetup &benchmark_setup) {
+	DuckDB db {};
+	StandardBufferManager buffer_manager {*db.instance, "/tmp/cached_http_fs_benchmark"};
+	auto s3fs = make_uniq<S3FileSystem>(buffer_manager);
+	auto cache_fs = make_uniq<CacheFileSystem>(std::move(s3fs));
+	auto client_context = make_shared_ptr<ClientContext>(db.instance);
 
-	std::cout << g_on_disk_cache_directory << std::endl;
-    FileSystem::CreateLocal()->RemoveDirectory(g_on_disk_cache_directory);
-    auto cache_fs = make_uniq<CacheFileSystem>(std::move(s3fs));
+	SetOpenerConfig(client_context, benchmark_setup);
+	ClientContextFileOpener file_opener {*client_context};
+	client_context->transaction.BeginTransaction();
 
-    auto client_context = make_shared_ptr<ClientContext>(db.instance);
-    SetSecretConfig(client_context);
-    ClientContextFileOpener file_opener {*client_context};
-    client_context->transaction.BeginTransaction();
-    auto file_handle = cache_fs->OpenFile("s3://duckdb-cache-fs/"
-                                    "lineitem.parquet",
-                                    FileOpenFlags::FILE_FLAGS_READ, &file_opener);	
-    const uint64_t file_size = cache_fs->GetFileSize(*file_handle);
-	
-    const size_t chunk_size = 32_MiB;
-    std::string buffer(chunk_size, '\0');
+	auto file_handle =
+	    cache_fs->OpenFile("s3://duckdb-cache-fs/lineitem.parquet", FileOpenFlags::FILE_FLAGS_READ, &file_opener);
+	const uint64_t file_size = cache_fs->GetFileSize(*file_handle);
 
-    auto read_sequential = [&]() {
-        uint64_t bytes_read = 0;
-        const auto now = std::chrono::steady_clock::now();
-        
-        while (bytes_read < file_size) {
-            size_t current_chunk = static_cast<size_t>(std::min(static_cast<uint64_t>(chunk_size), file_size - bytes_read));
-            cache_fs->Read(*file_handle, const_cast<char *>(buffer.data()), 
-                               current_chunk, bytes_read);
-            bytes_read += current_chunk;
-        }
+	const size_t chunk_size = 32_MiB;
+	std::string buffer(chunk_size, '\0');
 
-        const auto end = std::chrono::steady_clock::now();
-        const auto duration_sec = std::chrono::duration_cast<std::chrono::duration<double>>(end - now).count();
-        std::cout << "Sequential read of " << file_size << " bytes in " 
-                  << chunk_size << "-byte chunks takes " << duration_sec 
-                  << " seconds" << std::endl;
-    };
+	uint64_t bytes_read = 0;
+	const auto start = GetSteadyNowMilliSecSinceEpoch();
 
-	if(g_test_cache_type == duckdb::NOOP_CACHE_TYPE) {
-		read_sequential();
-		return;
-	} else if (g_test_cache_type == duckdb::ON_DISK_CACHE_TYPE) {
-		std::cout << "--------------------- Performing Uncached Read---------------------" << std::endl;
-		read_sequential();
-		std::cout << "--------------------- Performing cached  Read ---------------------" << std::endl;
-		read_sequential();
+	while (bytes_read < file_size) {
+		size_t current_chunk = std::min<size_t>(chunk_size, file_size - bytes_read);
+		cache_fs->Read(*file_handle, const_cast<char *>(buffer.data()), current_chunk, bytes_read);
+		bytes_read += current_chunk;
 	}
+
+	const auto end = GetSteadyNowMilliSecSinceEpoch();
+	const auto duration_millisec = end - start;
+	std::cout << "Sequential read of " << file_size << " bytes in " << chunk_size << "-byte chunks takes "
+	          << duration_millisec << " milliseconds" << std::endl;
 }
 
 } // namespace
@@ -82,18 +77,45 @@ void TestSequentialRead() {
 } // namespace duckdb
 
 int main(int argc, char **argv) {
+	// Ignore SIGPIPE, reference: https://blog.erratasec.com/2018/10/tcpip-sockets-and-sigpipe.html
 	std::signal(SIGPIPE, SIG_IGN);
-	duckdb::g_test_cache_type = duckdb::NOOP_CACHE_TYPE;
-	duckdb::g_on_disk_cache_directory = "/tmp/benchmark_cache";
-	// Warm Up Read();
-	// duckdb::TestSequentialRead();
-	
-	// Benchmark Noop cache Read();
-	duckdb::TestSequentialRead();
 
-	// Benchmark On Disk cache Read();
-	// It will perform uncached read first, then cached read.
-	duckdb::g_test_cache_type = duckdb::ON_DISK_CACHE_TYPE;
-	duckdb::TestSequentialRead();
+	const std::string disk_cache_directory = "/tmp/benchmark_cache";
+
+	// Warm up system resource (i.e. httpfs metadata cache, TCP congestion window, etc).
+	std::cout << "Starts to warmup read" << std::endl;
+	duckdb::FileSystem::CreateLocal()->RemoveDirectory(disk_cache_directory);
+	duckdb::BenchmarkSetup benchmark_setup;
+	benchmark_setup.cache_type = duckdb::NOOP_CACHE_TYPE;
+	benchmark_setup.profile_type = duckdb::NOOP_PROFILE_TYPE;
+	duckdb::TestSequentialRead(benchmark_setup);
+
+	// Benchmark httpfs (with no cache reader).
+	std::cout << "Starts with httpfs read with no cache" << std::endl;
+	duckdb::FileSystem::CreateLocal()->RemoveDirectory(disk_cache_directory);
+	benchmark_setup.cache_type = duckdb::NOOP_CACHE_TYPE;
+	benchmark_setup.profile_type = duckdb::TEMP_PROFILE_TYPE;
+	benchmark_setup.disk_cache_directory = disk_cache_directory;
+	duckdb::TestSequentialRead(benchmark_setup);
+
+	// Benchmark on-disk cache reader.
+	std::cout << "Starts on-disk cache read with no existing cache" << std::endl;
+	duckdb::FileSystem::CreateLocal()->RemoveDirectory(disk_cache_directory);
+	benchmark_setup.cache_type = duckdb::ON_DISK_CACHE_TYPE;
+	benchmark_setup.profile_type = duckdb::TEMP_PROFILE_TYPE;
+	benchmark_setup.disk_cache_directory = disk_cache_directory;
+	benchmark_setup.block_size = 2_MiB;
+	duckdb::TestSequentialRead(benchmark_setup);
+
+	// No delete cache directory to verify cache read.
+	std::cout << "Starts on-disk cache read with local cache" << std::endl;
+	benchmark_setup.cache_type = duckdb::ON_DISK_CACHE_TYPE;
+	benchmark_setup.profile_type = duckdb::TEMP_PROFILE_TYPE;
+	benchmark_setup.disk_cache_directory = disk_cache_directory;
+	duckdb::TestSequentialRead(benchmark_setup);
+
+	// Cleanup on-disk cache after benchmark.
+	duckdb::FileSystem::CreateLocal()->RemoveDirectory(disk_cache_directory);
+
 	return 0;
 }
