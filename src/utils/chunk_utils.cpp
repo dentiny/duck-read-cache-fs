e67c936191bb1ced846cafde0a5cdad72c4db1bd
dentiny <dentinyhao@gmail.com>
2025-12-08 08:12:50 +0000

    minor

--- src/utils/chunk_utils.cpp
+++ src/utils/chunk_utils.cpp
@@ -5,7 +5,7 @@ namespace duckdb {
 ChunkAlignmentInfo CalculateChunkAlignment(const ReadRequestParams &params) {
 	const idx_t aligned_start_offset = params.requested_start_offset / params.block_size * params.block_size;
 	const idx_t aligned_last_chunk_offset =
-	    ((params.requested_start_offset + params.requested_bytes_to_read - 1) / params.block_size) * params.block_size;
+	    (params.requested_start_offset + params.requested_bytes_to_read - 1) / params.block_size * params.block_size;
 	const idx_t subrequest_count = (aligned_last_chunk_offset - aligned_start_offset) / params.block_size + 1;
 
 	return ChunkAlignmentInfo {
