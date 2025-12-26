0965921d1e58cb3dd6120020b2faadf2fc331c5b
dentiny <dentinyhao@gmail.com>
2025-12-26 00:06:59 -0800

    Split header file constants (#357)

---
+++ src/utils/time_utils.cpp
@@ -0,0 +1,10 @@
+#include "include/time_utils.hpp"
+
+namespace duckdb {
+
+const uint64_t kMicrosToNanos = 1000ULL;
+const uint64_t kSecondsToMicros = 1000ULL * 1000ULL;
+const uint64_t kSecondsToNanos = 1000ULL * 1000ULL * 1000ULL;
+const uint64_t kMilliToNanos = 1000ULL * 1000ULL;
+
+} // namespace duckdb
