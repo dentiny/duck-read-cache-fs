c11a64f8d73aafdbf4ece8446d7a7e06962c7892
dentiny <dentinyhao@gmail.com>
2026-02-22 08:10:37 -0800

    Use STL regex to replace re2 (#428)

--- src/cache_exclusion_manager.cpp
+++ src/cache_exclusion_manager.cpp
@@ -1,28 +1,23 @@
 #include "cache_exclusion_manager.hpp"
 
-#include <utility>
-
-#include "duckdb/common/helper.hpp"
-
 namespace duckdb {
 
 void CacheExclusionManager::AddExclusionRegex(const string &regex) {
-	auto pattern = make_uniq<::duckdb_re2::RE2>(regex);
 	const concurrency::lock_guard<concurrency::mutex> lck(mu);
-	exclusion_regexes.emplace_back(std::move(pattern));
+	exclusion_regexes.emplace_back(regex);
+	exclusion_patterns.emplace_back(regex);
 }
 
 void CacheExclusionManager::ResetExclusionRegex() {
 	const concurrency::lock_guard<concurrency::mutex> lck(mu);
 	exclusion_regexes.clear();
+	exclusion_patterns.clear();
 }
 
 bool CacheExclusionManager::MatchAnyExclusion(const string &filepath) const {
-	// TODO(hjiang): Could be accessed by multiple threads and potentially be a bottleneck, could use shared pointer to
-	// improve.
 	const concurrency::lock_guard<concurrency::mutex> lck(mu);
 	for (const auto &cur_pattern : exclusion_regexes) {
-		if (RE2::PartialMatch(filepath, *cur_pattern)) {
+		if (std::regex_search(filepath, cur_pattern)) {
 			return true;
 		}
 	}
@@ -30,13 +25,8 @@ bool CacheExclusionManager::MatchAnyExclusion(const string &filepath) const {
 }
 
 vector<string> CacheExclusionManager::GetExclusionRegex() const {
-	vector<string> results;
 	const concurrency::lock_guard<concurrency::mutex> lck(mu);
-	results.reserve(exclusion_regexes.size());
-	for (const auto &cur_pattern : exclusion_regexes) {
-		results.emplace_back(cur_pattern->pattern());
-	}
-	return results;
+	return exclusion_patterns;
 }
 
 } // namespace duckdb
