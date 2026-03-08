4630823a668e5c98625bbd8ecebd36bcc1b96073
dentiny <dentinyhao@gmail.com>
2026-03-08 04:30:00 +0000

    add unaligned test

--- test/unittest/test_page_aligned_data_chunk.cpp
+++ test/unittest/test_page_aligned_data_chunk.cpp
@@ -23,3 +23,20 @@ TEST_CASE("PageAlignedDataChunk CopyTo copies data to destination", "[page_align
 
 	REQUIRE(std::memcmp(dest.data(), expected.data(), expected.length()) == 0);
 }
+
+TEST_CASE("PageAlignedDataChunk CopyTo with non-zero src_offset", "[page_aligned_data_chunk]") {
+	const idx_t page_size = GetFileSystemPageSize();
+	const string full = "hello world";
+
+	auto chunk = AllocatePageAlignedChunk(page_size);
+	std::memcpy(chunk.data(), full.data(), full.length());
+	chunk.length = full.length();
+
+	const idx_t src_offset = 6;
+	const idx_t copy_length = 5;
+	vector<char> dest(copy_length, 0);
+	chunk.CopyTo(dest.data(), src_offset, copy_length);
+
+	const string expected = "world";
+	REQUIRE(std::memcmp(dest.data(), expected.data(), copy_length) == 0);
+}
